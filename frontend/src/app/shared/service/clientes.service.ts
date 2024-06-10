4c4a5742198fa0043cc292d56953217266c82736
Victoria Fernandes <victoriafernandesgalvao@hotmail.com>
2024-06-10 18:38:20 -0300

    Implementando métodos em cliente-listagem, criando seletor e alterando cliente-service.

--- frontend/src/app/shared/service/clientes.service.ts
+++ frontend/src/app/shared/service/clientes.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Cliente } from '../model/cliente';
+import { Observable } from 'rxjs';
+import { ClienteSeletor } from '../seletor/cliente.seletor';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +12,30 @@ export class ClientesService {
 
   constructor(private httpClient: HttpClient) {}
 
+  consultarTodosClientes(): Observable<Array<Cliente>>{
+    return this.httpClient.get<Array<Cliente>>(this.API + '/todos')
+  }
 
-  //Métodos DE ClienteController aqui.
+
+  listarComSeletor(seletor: ClienteSeletor): Observable<Array<Cliente>> {
+    return this.httpClient.post<Array<Cliente>>(this.API + '/filtrar', seletor);
+  }
+
+  consultarVacinaID(idCliente: number): Observable<Cliente> {
+    return this.httpClient.get<Cliente>(this.API + '/consultar/' + idCliente )
+  }
+
+  excluir (clienteID: number): Observable<boolean> {
+    return this.httpClient.delete<boolean>(this.API + '/excluir/' + clienteID)
+  }
+
+
+  salvar(novoCliente: Cliente): Observable<Cliente> {
+    return this.httpClient.post<Cliente>(this.API + '/salvar', novoCliente)
+  }
+
+
+  editar(clienteEditado: Cliente): Observable<boolean>{
+    return this.httpClient.put<boolean>(this.API + '/alterar', clienteEditado)
+  }
 }
