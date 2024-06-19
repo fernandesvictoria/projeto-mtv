5879ccdba0fee01099bdb5aa5233898193a3c09b
Maria Leitão <malufln.99@gmail.com>
2024-06-18 22:51:51 -0300

    Implementação da tela de Cadastro e Navbar

--- frontend/src/app/queimas/queimas.module.ts
+++ frontend/src/app/queimas/queimas.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // Importar FormsModule
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { QueimaListagemComponent } from './queima-listagem/queima-listagem.component'; // Importar o componente da lista de queimas
 import { QueimasRoutingModule } from './queimas-routing.module';
+import { QueimaListagemComponent } from './queima-listagem/queima-listagem.component';
+import { QueimaDetalheComponent } from './queima-detalhe/queima-detalhe.component';
 
 @NgModule({
   declarations: [
-    QueimaListagemComponent
+    QueimaListagemComponent,
+    QueimaDetalheComponent
   ],
   imports: [
     CommonModule,
