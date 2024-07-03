6216a38f16993438849c35662e8610d90c157b5c
Maria Leitão <malufln.99@gmail.com>
2024-07-03 00:41:03 -0300

    fix: authLogin

--- backend/src/main/java/controller/ClienteController.java
+++ backend/src/main/java/controller/ClienteController.java
@@ -3,7 +3,6 @@ package controller;
 import java.util.List;
 
 import exception.CeramicaException;
-import filter.AuthFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
@@ -16,17 +15,13 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import model.entity.Cliente;
-import model.entity.Usuario;
 import model.seletor.ClienteSeletor;
 import service.ClienteService;
-import service.UsuarioService;
 
 @Path("/restrito/cliente")
 public class ClienteController {
 
 	private ClienteService service = new ClienteService();
-	private UsuarioService usuario = new UsuarioService();
-
 	@Context
 	private HttpServletRequest request;
 
@@ -35,22 +30,12 @@ public class ClienteController {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Cliente salvar(Cliente novoCliente) throws CeramicaException {
-		String idSessaoNoHeader = request.getHeader(AuthFilter.CHAVE_ID_SESSAO);
-		Usuario usuarioAutenticado = this.usuario.consultarPorIdSessao(idSessaoNoHeader);
-		if (usuarioAutenticado == null) {
-			throw new CeramicaException("Usuário não encontrado");
-		}
 		return service.salvar(novoCliente);
 	}
 
 	@DELETE
 	@Path("excluir/{id}")
 	public boolean excluir(@PathParam("id") int id) throws CeramicaException {
-		String idSessaoNoHeader = request.getHeader(AuthFilter.CHAVE_ID_SESSAO);
-		Usuario usuarioAutenticado = this.usuario.consultarPorIdSessao(idSessaoNoHeader);
-		if (usuarioAutenticado == null) {
-			throw new CeramicaException("Usuário não encontrado");
-		}
 		return service.excluir(id);
 	}
 
@@ -59,33 +44,18 @@ public class ClienteController {
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("/alterar")
 	public boolean alterar(Cliente clienteAlterado) throws CeramicaException {
-		String idSessaoNoHeader = request.getHeader(AuthFilter.CHAVE_ID_SESSAO);
-		Usuario usuarioAutenticado = this.usuario.consultarPorIdSessao(idSessaoNoHeader);
-		if (usuarioAutenticado == null) {
-			throw new CeramicaException("Usuário não encontrado");
-		}
 		return service.alterar(clienteAlterado);
 	}
 
 	@GET
 	@Path("/todos")
 	public List<Cliente> consultarTodasPessoas() throws CeramicaException {
-		String idSessaoNoHeader = request.getHeader(AuthFilter.CHAVE_ID_SESSAO);
-		Usuario usuarioAutenticado = this.usuario.consultarPorIdSessao(idSessaoNoHeader);
-		if (usuarioAutenticado == null) {
-			throw new CeramicaException("Usuário não encontrado");
-		}
 		return service.consultarTodosClientes();
 	}
 
 	@GET
 	@Path("consultar/{id}")
 	public Cliente consultarPorId(@PathParam("id") int id) throws CeramicaException {
-		String idSessaoNoHeader = request.getHeader(AuthFilter.CHAVE_ID_SESSAO);
-		Usuario usuarioAutenticado = this.usuario.consultarPorIdSessao(idSessaoNoHeader);
-		if (usuarioAutenticado == null) {
-			throw new CeramicaException("Usuário não encontrado");
-		}
 		return service.consultarPorId(id);
 	}
 
@@ -94,11 +64,6 @@ public class ClienteController {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Cliente> consultarComFiltro(ClienteSeletor seletor) throws CeramicaException {
-		String idSessaoNoHeader = request.getHeader(AuthFilter.CHAVE_ID_SESSAO);
-		Usuario usuarioAutenticado = this.usuario.consultarPorIdSessao(idSessaoNoHeader);
-		if (usuarioAutenticado == null) {
-			throw new CeramicaException("Usuário não encontrado");
-		}
 		return service.consultarComFiltro(seletor);
 	}
 }
